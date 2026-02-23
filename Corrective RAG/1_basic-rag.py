fba18420997b907e5af117da06bc2cb1dec12fda
chaudhary-pawan <Pc1498180@gmail.com>
2026-02-23 23:17:05 +0530

    Adding basic RAG

---
+++ Corrective RAG/1_basic-rag.py
@@ -0,0 +1,14 @@
+from typing import List, TypedDict
+import time
+
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.vectorstores import FAISS
+from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_core.documents import Document
+from langchain_core.prompts import ChatPromptTemplate
+
+from langgraph.graph import StateGraph, START, END
+from dotenv import load_dotenv
+
+load_dotenv()
